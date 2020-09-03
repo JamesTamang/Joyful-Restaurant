ea1d17405aa19a095568a4809f6329739a58ceac
nizer1215 <64226404+james1350@users.noreply.github.com>
2020-09-03 11:51:53 +0200

    almost done

--- java/script.js
+++ java/script.js
@@ -1,128 +1,135 @@
-function init(){
-    fetch("https://kea-alt-del.dk/t5/api/categories").then(r=>r.json()).then(
-        function(data){
-        categoriesReceived(data)
+function init() {
+    fetch("https://kea-alt-del.dk/t5/api/categories").then(r => r.json()).then(
+        function (data) {
+            categoriesReceived(data)
         }
-)
+    )
 }
 init();
 
-function categoriesReceived(cats){
+function categoriesReceived(cats) {
     createNavigation(cats);
     createSection(cats);
     fetchProducts();
 
 }
-function createSection(categories){
-    categories.forEach(category=>{
-                       const section = document.createElement("section");
-    section.setAttribute("id", category);
-    const h2=document.createElement("h2");
-    h2.textContent=category;
-    section.appendChild(h2);
- document.querySelector(".productList").appendChild(section);
-})
-                       }
-function catagoriesReceived(cat){
+
+function createSection(categories) {
+    categories.forEach(category => {
+        const section = document.createElement("section");
+        section.setAttribute("id", category);
+        const h2 = document.createElement("h2");
+        h2.textContent = category;
+        section.appendChild(h2);
+        document.querySelector(".productList").appendChild(section);
+    })
+}
+
+function catagoriesReceived(cat) {
     createNavigatin(cats);
 }
-function createNavigation(catagories){
-    catagories.forEach(cat=>{
-    const a=document.createElement("a");
-    a.textContent=cat;
-    a.setAttribute("href", `#${cat}`);
 
-    document.querySelector("nav").appendChild(a);
-})}
+function createNavigation(catagories) {
+    catagories.forEach(cat => {
+        const a = document.createElement("a");
+        a.textContent = cat;
+        a.setAttribute("href", `#${cat}`);
 
-
-function fetchProducts(){
-
-//template
-fetch("https://kea-alt-del.dk/t5/api/productlist")
-.then (function (response){
-    return response.json();
+        document.querySelector("nav").appendChild(a);
+    })
 }
-     )
-.then (function(data){
-    dataReceived(data);
-})
+
+
+function fetchProducts() {
+
+    //template
+    fetch("https://kea-alt-del.dk/t5/api/productlist")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            dataReceived(data);
+        })
 }
-function dataReceived(products){
+
+function dataReceived(products) {
     //loop
     products.forEach(showProduct)
 }
 
 //display each product individual
-function showProduct(myProduct){
-const template=document.querySelector("template").content;
+function showProduct(myProduct) {
+    const template = document.querySelector("#productTemplate").content;
 
-const copy=template.cloneNode(true);
-
-    const article=copy.querySelector("article");
-
-if(myProduct.vegetarian){
-    article.classList.add("vegetarian")
-}
-
-copy.querySelector(".full_name").textContent=myProduct.name;
-
-    copy.querySelector("p").textContent=myProduct.shortdescription;
-    copy.querySelector(".discountp").textContent=myProduct.discount;
-
-
- copy.querySelector(".price").textContent=myProduct.price;
+    const copy = template.cloneNode(true);
+    const img = copy.querySelector(".productImage");
+    img.setAttribute("src", `https://kea-alt-del.dk/t5/site/imgs/medium/${myProduct.image}-md.jpg`)
 
 
 
 
-    if (myProduct.discount){
- copy.querySelector(".discountp").classList.add("hidden");
-    }
-copy.querySelector(".full_name").textContent=myProduct.name;
-    if(myProduct.category==myProduct.category){
-        const parentElem=document.querySelector("section#"+myProduct.category);
-
-copy.querySelector("button").addEventListener("click",()=>{
-    fetch('https://kea-alt-del.dk/t5/api/product?id='+myProduct.id)
-    .then(res=>res.json())
-    .then(showDetails);
-})
-
-        const img=copy.querySelector(".productImage");
-img.setAttribute("src",'https://kea-alt-del.dk/t5/site/imgs/medium/' + myProduct.id
-                 +'-md.jpg')
-    const pasting = document.querySelector("section#"+myProduct.category);
-    pasting.appendChild(copy);
-
+    if (myProduct.soldout) {
+      const p=document.createElement("p");
+        p.textContent="Sold Out";
+        p.classList.add("soldout");
+        copy.querySelector("article").appendChild(p)
 
     }
+        if (!myProduct.discount) {
+ copy.querySelector(".discountp").classList.add("hidden")
+    }
+
+
+    if (myProduct.category == myProduct.category) {
+        const parentElem = document.querySelector("section#" + myProduct.category);
+    }
+
+const article = copy.querySelector("article");
+
+    if (myProduct.vegetarian) {
+        article.classList.add("vegetarian");
+    }
+
+    copy.querySelector(".full_name").textContent = myProduct.name;
+
+//buttin
+ copy.querySelector("#but").addEventListener("click", () => {
+            fetch(`https://kea-alt-del.dk/t5/api/product?id=` + myProduct.id)
+                .then(res => res.json())
+                .then(showDetails);
+        });
+    const parentElem = document.querySelector("section#" + myProduct.category);
+    parentElem.appendChild(copy)
+
+
+
+
+
+
 }
-const modal= document.querySelector(".modal-background");
-function showDetails(data){
-    modal.querySelector(".modal-name").textContent.data.name;
-    modal.querySelector(".modal-description").textContent=data.longdescription;
+const modal = document.querySelector(".modal-background");
 
-    modal.classList.remove("hide");
+function showDetails(data) {
+    modal.querySelector(".modal-name").textContent=data.name;
+    modal.querySelector(".modal-description").textContent = data.longdescription;
     modal.classList.remove("hide");
 }
 
 
-const veggifilter= document.querySelector("#veggifilter");
+const veggifilter = document.querySelector("#veggifilter");
 
 veggifilter.addEventListener("click", veggiFilterClicked);
 
-function veggiFilterClicked(){
-    const articles=document.querySelectorAll("article:not(.vegetarian)");
-    articles.forEach(elem=>{
-                     elem.classList.add("hidden");
-                     })
+function veggiFilterClicked() {
+    const articles = document.querySelectorAll("article:not(.vegetarian)");
+    articles.forEach(elem => {
+        elem.classList.toggle("hidden");
+    })
 }
 
 
 
 
-modal.addEventListener("click",()=>{
+modal.addEventListener("click", () => {
     modal.classList.add("hide");
 })
-
